8cc8e5a0a181a278d3f99d99506ccd35f7c73187
R1dge_PRIVATE <rridge939@gmail.com>
2020-11-25 15:19:44 +0900

    関数をModule.pyに分離，試験動作用のv2を作成．

---
+++ Module.py
@@ -0,0 +1,143 @@
+# ソースコードを見やすく，機能を使い回せるように，関数部分をモジュール化したい．
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#           --- 関数の定義 ---
+
+# 透視変換を行う
+def perspectiveTransform(img, pts_input, pts_output):   # (画像，オリジナルにおける四隅の座標，変換後の四隅の座標)
+    # "img.shape"：画像の情報。「高さ・幅・チャンネル数」を返す
+    raws, cols, ch = img.shape  # img.shapeで得た情報を順番にraws,cols,ch変数に格納
+    # cv2.getPerspectiveTransform()：透視写像行列を計算する
+    M = cv2.getPerspectiveTransform(pts_input, pts_output)  # M=透視写像行列
+    # cv.warpPerspective()：透視変換
+    outputImg = cv2.warpPerspective(img, M, (800, 800)) # outputImg=変換を行った画像
+    return outputImg
+
+# 点→多角形表示。碁盤を切り抜く四点を繋いだ四角形の可視化用。
+def drawCorner(img,pts):
+    outputImg = img.copy()
+    cv2.polylines(outputImg, [pts], True, (0,255,0), thickness=5, lineType=cv2.LINE_AA)
+    return outputImg
+
+# ノイズ処理を行う
+def reduceNoise(img, medianBoxSize, kernelSize):
+    # cv2.medianBlur()：メディアンフィルタ
+    cv2.medianBlur(img, medianBoxSize)
+    # np.ones(サイズ，データ型)：全要素が1の行列を生成。この場合kernelSize*kernelSizeの、全要素が1の行列が用意される。
+    kernel = np.ones((kernelSize, kernelSize), np.uint8)    #np.uint8：NumPyのデータ型のひとつ、符号なし8bit
+    # cv2.morphologyEx(画像，処理，構造要素)：“モルフォロジー”（画像上の図形に対して作用するシンプルな処理）関数
+        # cv2.MORPH_CLOSE：クロージング処理
+    outputImg = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    return outputImg
+
+# 交点位置の計算？
+def retCrossPoints(img):
+    crossPoints = []
+    # img.shape[B] = 画像の高さ　([1]だと幅、[2]だとチャンネル数)
+    interval = img.shape[0]/18  # interval = 画像の高さ/ 18……交点付与用の座標
+    for i in range(19): # in range(x)：x回繰り返す。この場合19回。C言語でいう「for(i=B; i<19; i++)」
+        row = []
+        for j in range(19): 
+            x = int(i*interval)
+            y = int(j*interval)
+            if i==0:
+                x += 7   # i=Bのとき、x座標+7    （端だけ交点をずらす）
+            if j==0:
+                y += 7   # jも同様
+            if i==18:
+                x -= 7  # i=18のとき、x座標-7   （端だけ交点をずらす）
+            if j==18:
+                y -= 7  # jも同様
+            row.append([y, x])  # row.append()：リスト末尾に要素を追加
+        crossPoints.append(row) # crossPoints=交点の座標がまとまったリスト  [[[x1,y1]，[x2,y2]，……]]
+    return crossPoints  
+
+# 交点の描画
+def drawCrossPoints(img):
+    outputImg = img.copy()
+    crossPoints = retCrossPoints(img)    
+    for p_row in crossPoints:
+        for p in p_row: 
+            # cv2.drawMarker(画像, 座標, 色, タイプ, サイズ, 薄さ, 線タイプ値)
+            cv2.drawMarker(outputImg, (p[0],p[1]), (0, 255, 0), cv2.MARKER_TILTED_CROSS, 10, 2)
+    return outputImg
+
+# 交点周囲（四角形）の描画
+def drawXP_Rect(img):
+    outputImg = img.copy()
+    crossPoints = retCrossPoints(img)    
+    for p_row in crossPoints:
+        for p in p_row:
+            cv2.rectangle(outputImg, (p[0]-4,p[1]-4),(p[0]+7,p[1]+7),(0,255,0),1)
+
+    return outputImg
+
+# 石の位置・種類の判別
+def checkStonePosition(img):
+    crossPoints = retCrossPoints(img)
+    conditionOfBoard = []
+    none = 10.0
+    blackStone = 80.0
+    whiteStone = 120.0
+
+    for p_row in crossPoints:
+        condition_row = []
+        for p in p_row:
+            # print("["+str(p_row.index(p)+1)+", "+str(crossPoints.index(p_row)+1)+"]")
+
+            # 交点の周囲（=drawCrossPointsによって付与される緑枠内）の全ピクセルのBGRを取得
+            rect = img[p[1]-3:p[1]+7, p[0]-3:p[0]+7]
+
+            # rect（=緑枠内の全ピクセルのBGR）の平均値を取得
+            colorAve = np.average(rect)
+            # p = [p[1],p[B]] = [y,x]
+            # print('colorAve' + "["+str(crossPoints.index(p_row)+1)+", "+str(p_row.index(p)+1)+"]" + ':' +str(colorAve))
+            
+            # 「目標の交点の、その周囲のRGB値の平均」(colorAve)が閾値を下回ってるかどうかで、石の有無を判別
+            if colorAve < none:
+                condition_row.append("N")
+            elif colorAve < blackStone:
+                condition_row.append("B")
+            elif colorAve > whiteStone:
+                condition_row.append("W")  
+        conditionOfBoard.append(condition_row)
+
+    # これに石の情報があった！！
+    # print(conditionOfBoard)
+    return conditionOfBoard
+
+# checkStonePositionの石情報リストをもとに、画像上に点を付与する。
+def drawTerritoryColor(img,territoryTable):
+    outputImg = img.copy()
+    crossPoints = retCrossPoints(img)
+    i = 0
+    for p_row in crossPoints:
+        j = 0
+        for p in p_row:
+            state = territoryTable[i][j]
+            if state=="B":
+                cv2.drawMarker(outputImg, (p[0],p[1]), (0, 0, 255), cv2.MARKER_TILTED_CROSS, 10, 2)
+            elif state=="W":
+                cv2.drawMarker(outputImg, (p[0],p[1]), (255, 0, 0), cv2.MARKER_TILTED_CROSS, 10, 2)
+            elif state=="N":
+                cv2.drawMarker(outputImg, (p[0],p[1]), (0, 255, 0), cv2.MARKER_CROSS, 10)
+            j += 1
+        i += 1
+    return outputImg
+
+# 仕組みはdrawTerritoryColorと同様。compareの内容に応じて画像上に点を付与する。
+def drawCompareStone(img,territoryTable):
+    outputImg = img.copy()
+    crossPoints = retCrossPoints(img)
+    i = 0
+    for p_row in crossPoints:
+        j = 0
+        for p in p_row:
+            state = territoryTable[i][j]
+            if str(state)=="False":
+                cv2.drawMarker(outputImg, (p[0],p[1]), (0, 255, 0), cv2.MARKER_TILTED_CROSS, 10, 2)
+            j += 1
+        i += 1
+    return outputImg
---
+++ PyTest_v2.py
@@ -0,0 +1,134 @@
+# 本当にこんなにインポートする必要があるのかは分からない．減らせるかもしれない．
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import csv
+import Module as MOD    # 関数部分を1つのファイルに分けた．使い回せるように．
+import os   # OSに限らず，この下の3行で実行時にコンソール内容を消去する．
+if os.name == 'nt': os.system('cls')
+else : os.system("clear")
+
+#           --- 処理開始 ---
+
+# ファイル指定と、指定ファイルの表示
+filename = './Input_IMG/IMG_5604.jpg'
+originalImg = cv2.imread(filename)
+cv2.namedWindow("originalImg", cv2.WINDOW_NORMAL)   # 画像がデカすぎるので縮小表示用
+cv2.imshow("originalImg",originalImg)
+cv2.waitKey(0)
+
+# 座標指定：[左上],[右上],[右下],[左下]
+pts = np.array([[801,503],[2617,156],[2810,2212],[789,2152]], np.int32)
+cornerOfGoBoard = np.float32(pts) # float32に型変換。透視変換行列の計算で必要になる。
+cornerOfImage = np.float32([[0,0], [800,0], [800,800], [0, 800]])
+
+# 切り抜き領域可視化
+cornerImg = MOD.drawCorner(originalImg,pts)
+for p in pts:
+    cv2.drawMarker(cornerImg, (p[0],p[1]), (0, 255, 0), cv2.MARKER_TILTED_CROSS, 50, 10)
+cv2.namedWindow("cornerImg", cv2.WINDOW_NORMAL)
+cv2.imshow("cornerImg",cornerImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/cornerImg.png', cornerImg)
+
+# 透視変換(Perspective Transform)
+boardImg = MOD.perspectiveTransform(originalImg, cornerOfGoBoard, cornerOfImage)
+cv2.imshow("Results/boardImg",boardImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/boardImg.png', boardImg)
+
+# 交点付与
+boardWithPointsImg = MOD.drawCrossPoints(boardImg)
+cv2.imshow("boardWithPointsImg",boardWithPointsImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/boardWithPointsImg.png', boardWithPointsImg)
+
+# ノイズ処理
+noiseReducedImg = MOD.reduceNoise(boardImg, 7, 5)
+cv2.imshow("noiseReducedImg",noiseReducedImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/noiseReducedImg.png', noiseReducedImg)
+
+# 確認用の表示(BGR→HSV変換)
+hsvImg = cv2.cvtColor(noiseReducedImg, cv2.COLOR_BGR2HSV)
+cv2.imshow("hsvImg",hsvImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/hsvImg.png', hsvImg)
+
+#マスク
+# beige = np.uint8([[[61,91,146]]])                   # これ使われてない
+# hsv_beige = cv2.cvtColor(beige,cv2.COLOR_BGR2HSV)   # これ使われてない……
+lower_beige = np.array([10,50,50])      # 閾値の下限
+upper_beige = np.array([30,255,255])    # 閾値の上限
+
+mask_beige = cv2.inRange(hsvImg, lower_beige, upper_beige)
+cv2.imshow("mask_beige",mask_beige)
+cv2.waitKey(0)
+cv2.imwrite('./Results/mask_beige.png', mask_beige)
+
+# ネガポジ
+mask_negaposi = cv2.bitwise_not(mask_beige)
+cv2.imshow("mask_negaposi",mask_negaposi)
+cv2.waitKey(0)
+cv2.imwrite('./Results/mask_negaposi.png', mask_negaposi)
+
+# 結果
+res = cv2.bitwise_and(boardImg,boardImg, mask= mask_negaposi)
+cv2.imshow("res",res)
+cv2.waitKey(0)
+cv2.imwrite('./Results/res.png', res)
+
+
+# resに点を付与
+resWithPointsImg = MOD.drawXP_Rect(res)
+cv2.imshow("resWithPointsImg",resWithPointsImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/resWithPointsImg.png', resWithPointsImg)
+
+stonePosition = MOD.checkStonePosition(res)
+# print(re.sub("[|[[|]|]|],", "", str(stonePosition).replace("], [","], \n[")))
+# stonePosition の情報を.csvに保存
+with open('./Results/stonePosition.csv', 'w', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(stonePosition)
+
+# print("stonePosition: \n" + str(stonePosition).replace("], [","], \n[") + "\n")
+
+# 結果
+resultImg = MOD.drawTerritoryColor(boardImg,stonePosition)
+cv2.imshow("resultImg",resultImg)
+cv2.waitKey(0)
+cv2.imwrite('./Results/resultIMG.png', resultImg)
+
+print("END")
+
+
+# 偽の碁盤情報（四隅の情報が違う）を用意、stonePositionと比較
+dummy = [
+["W", "B", "B", "W", "W", "N", "N", "N", "N", "N", "N", "N", "N", "N", "W", "W", "W", "W", "W"], 
+["N", "B", "W", "W", "N", "W", "N", "N", "N", "N", "N", "W", "W", "N", "W", "B", "W", "B", "B"], 
+["N", "B", "W", "B", "W", "W", "N", "N", "N", "W", "W", "B", "W", "W", "B", "B", "B", "B", "N"], 
+["N", "N", "B", "B", "B", "W", "W", "W", "W", "B", "B", "B", "W", "B", "B", "B", "B", "N", "N"], 
+["N", "N", "B", "W", "W", "B", "B", "B", "B", "B", "N", "B", "W", "B", "W", "W", "B", "N", "N"], 
+["N", "N", "B", "W", "W", "W", "B", "W", "B", "W", "B", "N", "B", "B", "W", "W", "B", "N", "N"], 
+["N", "N", "N", "B", "B", "W", "W", "W", "W", "W", "B", "B", "W", "W", "W", "B", "N", "N", "N"], 
+["N", "N", "B", "B", "W", "N", "W", "B", "B", "B", "B", "B", "W", "B", "W", "B", "B", "B", "B"], 
+["N", "N", "B", "W", "W", "N", "W", "B", "B", "W", "W", "B", "W", "B", "W", "W", "B", "W", "B"], 
+["N", "B", "W", "N", "N", "N", "W", "W", "B", "B", "W", "B", "B", "B", "B", "W", "B", "W", "W"], 
+["N", "B", "W", "N", "N", "N", "W", "B", "B", "B", "W", "W", "W", "W", "W", "B", "N", "W", "N"], 
+["B", "B", "W", "N", "N", "W", "B", "B", "W", "W", "N", "N", "N", "N", "N", "N", "W", "N", "N"], 
+["W", "B", "W", "W", "W", "W", "W", "W", "W", "B", "N", "N", "N", "N", "N", "W", "B", "W", "W"], 
+["W", "W", "W", "B", "B", "B", "W", "B", "B", "W", "W", "N", "N", "N", "N", "W", "B", "W", "W"], 
+["N", "W", "B", "B", "B", "B", "B", "N", "B", "W", "W", "N", "N", "N", "W", "B", "B", "B", "W"], 
+["W", "W", "W", "W", "W", "B", "N", "B", "N", "B", "B", "W", "W", "W", "W", "W", "B", "B", "W"], 
+["B", "W", "B", "W", "W", "B", "N", "W", "B", "B", "B", "W", "B", "B", "W", "W", "W", "B", "B"], 
+["B", "B", "B", "B", "B", "N", "N", "N", "N", "N", "B", "B", "N", "B", "B", "W", "B", "B", "N"], 
+["W", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "N", "B", "N", "N", "W"]]
+
+# ndarrayを比較演算子で比較すると，各要素に対して一致してるかどうかの真理値を返す．
+compare = np.array(stonePosition) == np.array(dummy)    
+# 比較結果を画像に重ねる．
+resultDummyCompare = MOD.drawCompareStone(boardImg, compare)
+cv2.imshow("compare", resultDummyCompare)
+cv2.waitKey(0)
