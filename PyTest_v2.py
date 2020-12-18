78d82ebb8e1888b538a9084f740c99d579e53e2f
NITOita_S1630 <s1630@oita.kosen-ac.jp>
2020-12-18 14:27:13 +0900

    画像変更

--- PyTest_v2.py
+++ PyTest_v2.py
@@ -12,14 +12,13 @@ else : os.system("clear")
 #           --- 処理開始 ---
 
 # ファイル指定と、指定ファイルの表示
-# originalImg = cv2.imread("./Input_IMG/DSC_0041.jpg")
 originalImg = cv2.imread("./Input_IMG/DSC_0170.jpg")
 cv2.namedWindow("originalImg", cv2.WINDOW_NORMAL)   # 画像がデカすぎるので縮小表示用
 cv2.imshow("originalImg",originalImg)
 cv2.waitKey(0)
 
 # 座標指定：[左上],[右上],[右下],[左下]
-pts = np.array([[]], np.int32)
+pts = np.array([[1043,170],[2749,170],[2755,1822],[947,1772]], np.int32)
 # pts = np.array([[1126,250],[2735,105],[2745,1818],[1103,1711]], np.int32)
 cornerOfGoBoard = np.float32(pts) # float32に型変換。透視変換行列の計算で必要になる。
 cornerOfImage = np.float32([[0,0], [800,0], [800,800], [0, 800]])
