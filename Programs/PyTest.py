91d53ae2d8aace51c313278b9ddccc9aeca39858
local_s1630 <s1630@oita.kosen-ac.jp>
2021-01-28 16:11:51 +0900

    readme edit

--- Programs/PyTest.py
+++ Programs/PyTest.py
@@ -5,222 +5,211 @@ import matplotlib.pyplot as plt
 import re
 import csv
 import Module as MOD    # 関数部分を1つのファイルに分けた．使い回せるように．
-import os   # OSに限らず，この下の3行で実行時にコンソール内容を消去する．
+# OSに限らず，この下の3行で実行時にコンソール内容を消去する．clearとかclsとかの実行が面倒なので。
+import os   
 if os.name == 'nt': os.system('cls')
 else : os.system("clear")
 
 #           --- 処理開始 ---
 
-# ファイル指定と、指定ファイルの表示
+#? ファイル指定
 print("Input filename...")
-filename = str(input()) # ファイル名指定
-print("\n")
+filename = str(input()) # 入力input()を文字列変換し、filenameに格納
 
-print("Input threshold_black (default=58)...")
-threshold_black = str(input())
-if(len(threshold_black)==0):
-    threshold_black=58
-else:
-    threshold_black=int(threshold_black)
-print(threshold_black)
-
-print("Input threshold_white (default=158)...")
-threshold_white = str(input())
-if(len(threshold_white)==0):
-    threshold_white=158
-else:
-    threshold_white=int(threshold_white)
-print(threshold_white)
-
-# print("Threshold_black = " + str(threshold_black))
-
-
-
-
-
-
-
-if not(os.path.exists("./Results/"+filename)): os.makedirs("./Results/"+filename)   # ファイル名のディレクトリを作る（画像ごとにいちいち座標とか再設定するのが面倒になった）
-originalImg = cv2.imread("./Input_IMG/"+filename+".jpg")
-
-# 画像名のディレクトリにあるptlist.txtをもとに切り抜く四隅を指定（座標指定：[左上],[右上],[右下],[左下]）
-ptlist=[]
-if not(os.path.exists('./Results/'+filename+'/ptlist.txt')): 
-    path = './Results/'+filename+'/ptlist.txt'
-    f = open(path, 'w')
-    f.write('')  # 何も書き込まなくてファイルは作成されました
-    f.close()
-    print("WARNING: ./Results/"+filename+"/ptlist.txt generated.")
+if not(os.path.isfile("../RawData/Input_IMG/"+filename+".jpg")):
+    print("file not found.")
+    print("Program terminated.")
     quit()
 
-for l in open('./Results/'+filename+'/ptlist.txt').readlines():
+
+if not(os.path.exists("../RawData/Results/"+filename)): # もしファイル名のフォルダが無い場合
+    os.makedirs("../RawData/Results/"+filename)   # ファイル名のディレクトリを作る（画像ごとにいちいち座標とか再設定するのが面倒になった）
+    print("WARNING: ../RawData/Results/"+filename+"/ generated.")
+
+if not(os.path.exists('../RawData/Results/'+filename+'/ptlist.txt')): # もしptlist.txtが作成されていない場合
+    path = '../RawData/Results/'+filename+'/ptlist.txt'
+    f = open(path, 'w')
+    f.write('')  # ptlist.txtに何も書き込まない（空のptlist.txtを作成する）
+    f.close()
+    print("WARNING: ../RawData/Results/"+filename+"/ptlist.txt generated.")
+    print("Program terminated.")
+    quit()
+
+
+def_black = 52
+def_white = 158
+
+print("Input threshold_black (default="+str(def_black)+")...")
+threshold_black = str(input())
+if(len(threshold_black)==0):
+    threshold_black=def_black
+else:
+    threshold_black=int(threshold_black)
+
+print("Input threshold_white (default="+str(def_white)+")...")
+threshold_white = str(input())
+if(len(threshold_white)==0):
+    threshold_white=def_white
+else:
+    threshold_white=int(threshold_white)
+
+
+#? 指定した画像の情報を、変数originalImgに代入
+originalImg = cv2.imread("../RawData/Input_IMG/"+filename+".jpg")
+
+#? 画像名のディレクトリにあるptlist.txtをもとに切り抜く四隅を指定（座標指定：[左上],[右上],[右下],[左下]）
+ptlist=[]
+for l in open('../RawData/Results/'+filename+'/ptlist.txt').readlines():
     data = l[:-1].split(',')
     ptlist += [[int(data[0]),int(data[1])]]
 
 pts = np.array(ptlist, np.int32)
 cornerOfGoBoard = np.float32(pts) # float32に型変換。透視変換行列の計算で必要になる。
-cornerOfImage = np.float32([[0,0], [800,0], [800,800], [0, 800]])
+cornerOfImage = np.float32([[0,0], [800,0], [800,800], [0, 800]]) # 変換後の四角形の形指定。この場合800x800pxの正方形
 
-# 切り抜き領域可視化
+#? 切り抜き領域可視化
 cornerImg = MOD.drawCorner(originalImg,pts)
 for p in pts:
     cv2.drawMarker(cornerImg, (p[0],p[1]), (0, 255, 0), cv2.MARKER_TILTED_CROSS, 50, 10)
-# cv2.namedWindow("cornerImg", cv2.WINDOW_NORMAL)
+# cv2.namedWindow("cornerImg", cv2.WINDOW_NORMAL) # リサイズ可能ウィンドウの生成。元画像はサイズが大きすぎるので。
 # cv2.imshow("cornerImg",cornerImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/cornerImg.jpg', cornerImg)
+cv2.imwrite('../RawData/Results/' + filename + '/cornerImg.jpg', cornerImg)
 
-# 透視変換(Perspective Transform)
+#? 透視変換(Perspective Transform)
 boardImg = MOD.perspectiveTransform(originalImg, cornerOfGoBoard, cornerOfImage)
 # cv2.imshow("Results/boardImg",boardImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/boardImg.jpg', boardImg)
+cv2.imwrite('../RawData/Results/' + filename + '/boardImg.jpg', boardImg)
 
-# BGR→GRAY変換
+#? BGR→GRAY変換
 GRAYImg = cv2.cvtColor(boardImg, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("GRAYImg",GRAYImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/GRAYImg.jpg', GRAYImg)
+cv2.imwrite('../RawData/Results/' + filename + '/GRAYImg.jpg', GRAYImg)
 
 
-kernelSize = 35
-median = cv2.medianBlur(GRAYImg,kernelSize)
-cv2.imwrite('./Results/' + filename + '/median.jpg', median)
-blur = cv2.blur(GRAYImg,(kernelSize,kernelSize))
-cv2.imwrite('./Results/' + filename + '/blur.jpg', blur)
+#? フィルタのサイズ
+    #! メディアンフィルタの際は、カーネルサイズは奇数でないとエラーが出る（偶数だと中央値が1つに決まらないため）。
+kernelSize = 35 
 
-# ノイズ処理
+#? 同じカーネルサイズでの、メディアンフィルタ適用画像（median）と平均化フィルタ適用画像（blur）を生成。
+    #! 確認用。後ろの処理で使ってないから、消してもいい。
+# median = cv2.medianBlur(GRAYImg,kernelSize)
+# cv2.imwrite('../RawData/Results/' + filename + '/median.jpg', median)
+# blur = cv2.blur(GRAYImg,(kernelSize,kernelSize))
+# cv2.imwrite('../RawData/Results/' + filename + '/blur.jpg', blur)
 
-noiseReducedImg = cv2.medianBlur(GRAYImg,kernelSize)
+
+
+#? ノイズ処理
+noiseReducedImg = cv2.medianBlur(GRAYImg,kernelSize) # この場合、メディアンフィルタを適用しただけ。上のmedianと同じものが出来上がる。
 # noiseReducedImg = cv2.blur(GRAYImg,(kernelSize,kernelSize))
 # cv2.imshow("noiseReducedImg",noiseReducedImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/noiseReducedImg.jpg', noiseReducedImg)
+cv2.imwrite('../RawData/Results/' + filename + '/noiseReducedImg.jpg', noiseReducedImg)
 
-# 結果に重ねる用の，カラー画像に変換したnoiseReducedImg(GRAY)
+
+#? グレースケール画像に色付きのマーカーを付与しても白黒表示になってしまうため、再度カラー画像に変換。
+    #! 結果表示で使うだけだから、なくてもいい。
 GRAY_to_COLOR_noiseReduced = cv2.cvtColor(noiseReducedImg, cv2.COLOR_GRAY2BGR)
 GRAY_to_COLOR = cv2.cvtColor(GRAYImg,cv2.COLOR_GRAY2BGR)
 
 
-# 交点付与（これ自体はなくてもいいが，取得領域の確認用）
+#? ノイズ処理画像に交点付与（取得領域の確認用）
 boardWithAreaImg = MOD.drawXP_Rect(GRAY_to_COLOR_noiseReduced)
 # cv2.imshow("boardWithAreaImg",boardWithAreaImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/boardWithAreaImg.jpg', boardWithAreaImg)
+cv2.imwrite('../RawData/Results/' + filename + '/boardWithAreaImg.jpg', boardWithAreaImg)
 
+#? 射影変換後の正方形画像に交点付与（取得領域の確認用）
 color_boardWithAreaImg = MOD.drawXP_Rect(boardImg)
 # cv2.imshow("color_boardWithAreaImg",color_boardWithAreaImg)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/color_boardWithAreaImg.jpg', color_boardWithAreaImg)
+cv2.imwrite('../RawData/Results/' + filename + '/color_boardWithAreaImg.jpg', color_boardWithAreaImg)
 
-# 石の有無を計算
+#? 石の有無を計算
 stonePosition_NoMask = MOD.checkStonePosition(noiseReducedImg, threshold_black, threshold_white) 
 stonePosition_NoMask_ALT = MOD.checkStonePosition_ALT(noiseReducedImg, threshold_black, threshold_white) 
-# noiseReducedに重ねて計算結果を表示
+
+#? noiseReducedに重ねて計算結果を表示
 result_noiseReduced = MOD.drawTerritoryColor(GRAY_to_COLOR_noiseReduced,stonePosition_NoMask)
 # cv2.imshow("result_noiseReduced",result_noiseReduced)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/result_noiseReduced.jpg', result_noiseReduced)
+cv2.imwrite('../RawData/Results/' + filename + '/result_noiseReduced.jpg', result_noiseReduced)
 
-# 盤面画像に重ねて計算結果を表示
+#? boardImg(グレー)に重ねて計算結果を表示
 result = MOD.drawTerritoryColor(GRAY_to_COLOR,stonePosition_NoMask)
 # cv2.imshow("result",result)
 # cv2.waitKey(0)
-cv2.imwrite('./Results/' + filename + '/result.jpg', result)
+cv2.imwrite('../RawData/Results/' + filename + '/result.jpg', result)
 
-#! 白のしきい値が拾ってる範囲を図に
-inRange_WHITE = cv2.inRange(noiseReducedImg,threshold_white,255)
+#! 白のしきい値が拾ってる範囲を示した画像。論文用に作っただけ
+# inRange_WHITE = cv2.inRange(noiseReducedImg,threshold_white,255)
 # cv2.imshow("inRange_WHITE",inRange_WHITE)
 # cv2.waitKey(0)
-# cv2.imwrite('./Results/' + filename + '/inRange_WHITE.jpg', inRange_WHITE)
+# cv2.imwrite('../RawData/Results/' + filename + '/inRange_WHITE.jpg', inRange_WHITE)
 
 # 結果
 # resultImg = MOD.drawTerritoryColor(GRAY_to_COLOR,stonePosition)
 # cv2.imshow("resultImg",resultImg)
 # cv2.waitKey(0)
-# cv2.imwrite('./Results/' + filename + '/resultIMG.jpg', resultImg)
+# cv2.imwrite('../RawData/Results/' + filename + '/resultIMG.jpg', resultImg)
 
-if not(os.path.exists('./Results/' + filename + '/stonePosition.csv')): 
-    path = './Results/' + filename + '/stonePosition.csv'
+
+#? 結果をCSVに書き込み。上2つのif文は、csvファイルが無かったら作成する処理。
+if not(os.path.exists('../RawData/Results/' + filename + '/stonePosition.csv')): 
+    path = '../RawData/Results/' + filename + '/stonePosition.csv'
     f = open(path, 'w')
     f.write('')  # 何も書き込まなくてファイルは作成されました
     f.close()
-    print("WARNING: ./Results/" + filename + '/stonePosition.csv generated!')
+    print("WARNING: ../RawData/Results/" + filename + '/stonePosition.csv generated!')
 
-if not(os.path.exists('./Results/' + filename + '/' + filename + '.csv')): 
-    path = './Results/' + filename + '/' + filename + '.csv'
+if not(os.path.exists('../RawData/Results/' + filename + '/' + filename + '.csv')): 
+    path = '../RawData/Results/' + filename + '/' + filename + '.csv'
     f = open(path, 'w')
     f.write('')  # 何も書き込まなくてファイルは作成されました
     f.close()
-    print("WARNING: ./Results/" + filename + '/' + filename + '.csv generated!')
+    print("WARNING: ../RawData/Results/" + filename + '/' + filename + '.csv generated!')
 
 
-with open('./Results/' + filename + '/stonePosition.csv', 'w', newline="") as file: # CSVに書き込み
+with open('../RawData/Results/' + filename + '/stonePosition.csv', 'w', newline="") as file: # CSVに書き込み
     writer = csv.writer(file, lineterminator='\n')
     writer.writerows(stonePosition_NoMask)
 
-with open('./Results/' + filename + '/stonePosition_ALT.csv', 'w', newline="") as file: # CSVに書き込み
+with open('../RawData/Results/' + filename + '/stonePosition_ALT.csv', 'w', newline="") as file: # CSVに書き込み
     writer = csv.writer(file, lineterminator='\n')
     writer.writerows(stonePosition_NoMask_ALT)
 
 
+    #! 処理とは関係ない。結果を数える用の処理。
+#     # ファイル名.csvに，正常な碁石の配置を記録しておくことで，一致した確率が求められる．
+# with open('../RawData/Results/' + filename + '/' + filename + '.csv', 'r', newline="") as f: # 偽の碁盤情報をdummyPosition.csvから取得，比較を行う
+#     reader = csv.reader(f) 
+#     l = [row for row in reader]
+#     # ndarrayを比較演算子で比較すると，各要素に対して一致してるかどうかの真理値を返す．
 
-    # ファイル名.csvに，正常な碁石の配置を記録しておくことで，一致した確率が求められる．
-with open('./Results/' + filename + '/' + filename + '.csv', 'r', newline="") as f: # 偽の碁盤情報をdummyPosition.csvから取得，比較を行う
-    reader = csv.reader(f) 
-    l = [row for row in reader]
-    # ndarrayを比較演算子で比較すると，各要素に対して一致してるかどうかの真理値を返す．
+#     # csv上の石の数
+#     BLACK_c = 0
+#     WHITE_c = 0
+#     BLACK_d = 0
+#     WHITE_d = 0
 
-    # csv上の石の数
-    BLACK_c = 0
-    WHITE_c = 0
-    BLACK_d = 0
-    WHITE_d = 0
+#     for R_csv in l: 
+#         BLACK_c += R_csv.count('B')
+#         WHITE_c += R_csv.count('W')
 
-    for R_csv in l: 
-        BLACK_c += R_csv.count('B')
-        WHITE_c += R_csv.count('W')
+#     # システムが検知(detect)した石の検知数
+#     for R_detected in stonePosition_NoMask:
+#         BLACK_d += R_detected.count('B')
+#         WHITE_d += R_detected.count('W')
 
-    # システムが検知(detect)した石の検知数
-    for R_detected in stonePosition_NoMask:
-        BLACK_d += R_detected.count('B')
-        WHITE_d += R_detected.count('W')
-
-    # # 比較結果を画像に重ねる．
-    compare = np.array(stonePosition_NoMask) == np.array(l)
-    # print(compare)
-    resultCompare = MOD.drawCompareStone(boardImg, compare)
-    # cv2.imshow("compare", resultCompare)
-    # cv2.waitKey(0)
-    cv2.imwrite('./Results/' + filename + '/resultCompare.jpg', resultCompare)
-
-kernel_high_pass = np.array([
-                            [-1, -1, -1],
-                            [-1,  8, -1],
-                            [-1, -1, -1]
-                            ], np.float32)
-
-img_high_pass = cv2.filter2D(GRAYImg, -1, kernel_high_pass)
-cv2.imwrite('./Results/' + filename + '/HighPass.jpg', img_high_pass)
-
-cimg = cv2.cvtColor(inRange_WHITE,cv2.COLOR_GRAY2BGR)
-
-circles = cv2.HoughCircles(inRange_WHITE,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=0,maxRadius=300)
-
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-cv2.imwrite('./Results/' + filename + '/detected circles.jpg',cimg)
-
-ret, img_otsu = cv2.threshold(GRAYImg, 0, 255, cv2.THRESH_OTSU)
-cv2.imwrite('./Results/' + filename + '/img_otsu.jpg',img_otsu)
-
-print(ret)
-
+#     # # 比較結果を画像に重ねる．
+#     compare = np.array(stonePosition_NoMask) == np.array(l)
+#     # print(compare)
+#     resultCompare = MOD.drawCompareStone(boardImg, compare)
+#     # cv2.imshow("compare", resultCompare)
+#     # cv2.waitKey(0)
+#     cv2.imwrite('../RawData/Results/' + filename + '/resultCompare.jpg', resultCompare)
 
 print("END")
--- README.md
+++ README.md
@@ -1,16 +1,50 @@
 # Igo-Python
-このソフトはどんなもので，何ができるのかを書く.
+画像から，碁石の配置を識別する
 
 # Dependence
 Python 3.8.5  
-必要なパッケージは./Programs/requirements.txtに記述してある。  
-`pip install -r requirements.txt`でまとめてインストールできるはず。
+※WSLのUbuntuに最初から入っていた。`python3`で実行できる  
+（WSLじゃない、Visual StudioのPythonとか、Windowsに直接Pythonを入れるとかの環境で実行確認はしてない）
+  
+必要なパッケージは、./Programs/requirements.txtに記述してある。  
+`pip install -r requirements.txt`でまとめてインストールできるはず。  
+  
+※プログラム内のコメントは、Visual Studio Codeの拡張機能"Better Comments"に合わせた書き方をしています。  
+　通常の行コメント(#)に、#!、#?、#TODO 、と書くと色を変えてくれる機能です。  
+　プログラムの一部をコメントアウトするとき、行末にコメントするときは基本的に通常通り、  
+　コメントだけの行は基本的に"Better Comments"に合わせた書き方にしています。
 
 # Setup
-セットアップ方法を書く．用意するハードウェアとソフトウェアをセットアップするためのコマンドを記載する
+- パッケージのインストールについて：省略。上に書いた。
+- 実行前に
+    - 使用する画像(.jpg)を、RawData/Input_IMG/に入れておくこと
+        - ファイル名入力のときに拡張子まで打つのが面倒だった、ってだけでJPGに統一。書き換えればPNGも読み込める。
+
 
 # Usage
-使い方．なるべく具体的に書く．サンプルも書く
+- `python3 pyTest.py`
+    - Input filename...：Input_IMGに入れた画像名を入力
+        - ファイルが無ければ、プログラムは終了する。
+        - 指定したファイルが初めて使用するものであれば、以下の処理を行ってプログラムは終了する
+            - RawData/Resultsに、画像名のディレクトリを作成
+            - 作成されたディレクトリ内に、ptlist.txtを作成
+                - このtxtには、盤面の左上、右上、右下、左下、の順で座標を書いておく必要がある。
+                - 改行して書くこと（既に作成済みのptlist.txtを参考に）。
+    - Input threshold_black (default=52)
+        - 黒石のしきい値の指定。何も打たずにEnterを押すと、def_blackの値（この場合52）が適用される
+    - Input threshold_black (default=158)
+        - 白石のしきい値の指定。何も打たずにEnterを押すと、def_whiteの値（この場合158）が適用される
+
+- 生成されるファイルについて
+    - cornerImg：元画像と、ptlistで指定した四角形を重ねた画像
+    - boardImg：ptlistの四角形を、800x800の正方形に変換した画像
+    - GRAYImg：boardImgのグレースケール変換
+    - noiseReducedImg：GRAYImgのノイズ処理適用後
+    - boardWithAreaImg：noiseReducedImg上に、輝度値を取得する領域を緑色の四角形で可視化
+    - color_boardWithAreaImg：boardImg上に、同じ緑色の四角形を重ねた
+    - result_noiseReduced：noiseReducedImg上に、識別結果のマーカーを重ねた
+    - result：グレースケール化したboardImg上に、識別結果のマーカーを重ねた
+
 
 # Authors
 作者を明示する．
@@ -27,23 +61,6 @@ Python 3.8.5
 
 
 # メモ
-- /Contour_Detect/board_detection.py
-    - 碁盤の輪郭を検出して切り取り、あとはPyTest.py(初期ver)と同様の処理を適用。
-- /FilterTest
-    - 画像処理をいろいろ試してるだけ．たぶんそのうち消す．
-- /Input_IMG
-    - 入力画像をまとめておく用
-- /Results
-    - 途中経過を含む、出力画像の保存場所
-- Module.py
-    - PyTest.pyが見づらくなってきたため，同ファイル上で定義している関数を別ファイルに纏めた．v2で使用している．
-- PyTest.py
-    - メインのやつ．いい加減ファイル名を変えよう．
-    - 四隅を指定して盤面を切り取り、点を付与して点周囲の色情報から碁石の有無を判別。
-- PyTest_v2.py
-    - PyTestと全く同じもの．
-    - PyTestの関数部を別ファイル(Module.py)に分けて，それを参照している．
-
 ---
 - プログラムは殆どが使いまわし。以下参考リンク
     - 碁盤の状態検出の参考にした
